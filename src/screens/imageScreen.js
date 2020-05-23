0b8b1f869ab4fbdd4645585a8ad8383e892f9ea4
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-05-23 22:20:42 +0530

    Create imageScreen.js

---
+++ src/screens/imageScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const imageScreen = () => {
+  return <Text>ImageScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default imageScreen;
