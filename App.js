4e48058092fc37db47b5e943f3121029863c23f0
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-05-24 22:11:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,12 +4,15 @@ import HomeScreen from './src/screens/HomeScreen';
 import componentsScreen from './src/screens/componentsScreen';
 import listScreen from './src/screens/listScreen';
 import imageScreen from './src/screens/imageScreen';
+import CounterScreen from './src/screens/counterScreen';
+import CouterScreen from './src/screens/counterScreen';
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     components: componentsScreen,
     List: listScreen,
     Image: imageScreen,
+    Count: CouterScreen,
   },
   {
     initialRouteName: 'Home',
