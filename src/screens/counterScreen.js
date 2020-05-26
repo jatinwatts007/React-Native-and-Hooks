71c8838705c0b813c684e17cbf0da7665c7c658a
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-05-26 22:23:42 +0530

    Update counterScreen.js

--- src/screens/counterScreen.js
+++ src/screens/counterScreen.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+const one = 1;
+const reducer = (state, action) => {
+  if (action.counterToChange) {
+    return { ...state, counter: state.counter + action.amount };
+  } else {
+    return counter;
+  }
+};
 
 const CouterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
+  //const [counter, setCounter] = useState(0);
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ counterToChange: 'counter', amount: one });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ counterToChange: 'counter', amount: -1 * one });
         }}
       />
       <Text>Current Count:{counter}</Text>
