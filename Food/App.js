5d9434eb8e325716924454dc1230aa2774f20c56
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-05-29 23:13:28 +0530

    Update App.js

--- Food/App.js
+++ Food/App.js
@@ -1,9 +1,11 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import SearchScreen from './src/screens/searchScreen';
+import ResultScreen from './src/screens/resultScreen';
 const navigator = createStackNavigator(
   {
     Screen: SearchScreen,
+    Result: ResultScreen,
   },
   {
     initialRouteName: 'Screen',
